ac546f5c7ebf77fb761485a73b80580f28127486
Dhwaneel Pandya <dhwaneel10@gmail.com>
2026-03-20 05:21:25 +0530

    Fix Gemini1

--- ml/gemini_chat.py
+++ ml/gemini_chat.py
@@ -1,15 +1,15 @@
 import google.generativeai as genai
-import os
+import streamlit as st
 
-genai.configure(api.key=st.secrets["AIzaSyAZxNwkAo_dNldwf0aRzecHmabOOg4J8TI"])
-
+# Correct way
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 def ask_ai(prompt):
     try:
         model = genai.GenerativeModel("gemini-2.0-flash")
-        
         response = model.generate_content(prompt)
-        
-        return response.text
+
+        return response.text if response else "No response from AI"
+
     except Exception as e:
         return f"AI Error: {str(e)}"
